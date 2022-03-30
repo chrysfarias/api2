0647b22669996257d648eb59dfb0135e2d2c7bc6
chrystian farias <chrystianfarias@hotmail.com>
2022-03-30 16:24:36 -0400

    Iniciando conexão com o Mysql

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using api2.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace api2
 {
@@ -28,6 +30,12 @@ namespace api2
         {
 
             services.AddControllers();
+            // Início conexão com o banco de dados   
+            string stringDeConexao = Configuration.GetConnectionString("conexaoMySQL");
+
+            services.AddDbContext<DataContext>(opt => opt.UseMySql(stringDeConexao, ServerVersion.AutoDetect(stringDeConexao)));
+            // Término da conexão com o banco de dados
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "api2", Version = "v1" });
