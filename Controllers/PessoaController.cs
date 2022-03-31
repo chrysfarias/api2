064434cef8eb3d336100ab77c9e90fdd65072681
chrystian farias <chrystianfarias@hotmail.com>
2022-03-31 09:30:38 -0400

    Controller & programação assincrona

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -1,14 +1,35 @@
+using System.Net;
 using System.Threading.Tasks;
 using api2.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using api2.Models;
 
-namespace api2.Controllers
+
+namespace api2.Controllers 
 {
     [Controller]
     [Route("[controller]")]
-    public class PessoaController
+
+    public class PessoaController : ControllerBase
     {
+        private DataContext dc;
+
+        public PessoaController(DataContext context)
+        {
+            this.dc = context;
+
+        }
+
+        [HttpPost("api")]
+        public async Task<ActionResult> cadastrar([FromBody] Pessoa p)
+        {
+            dc.pessoa.Add(p);
+            await dc.SaveChangesAsync();
+            return Created("Objeto pessoa",p);
+
+        }
+
          [HttpGet("oi")]
         public string oi ()
         {
